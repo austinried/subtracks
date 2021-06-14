1b78acd5faf7c87755ed562739403d94eb77b1d8
austinried <4966622+austinried@users.noreply.github.com>
2021-06-14 11:14:58 +0900

    now playing layout mock-up

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
+import { AppRegistry, LogBox } from 'react-native';
 
-import {AppRegistry} from 'react-native';
+// ignore recoil's timer warning on android:
+// https://github.com/facebookexperimental/Recoil/issues/1030
+LogBox.ignoreLogs(["timer"]);
+
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
 
 AppRegistry.registerComponent(appName, () => App);
