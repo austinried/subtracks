b51d0eb685a926fff3e4309f8d517317d65241e8
austinried <4966622+austinried@users.noreply.github.com>
2021-06-15 09:07:43 +0900

    first pass api client

--- App.tsx
+++ App.tsx
@@ -11,3 +11,15 @@ const App = () => {
 }
 
 export default App;
+
+import { SubsonicApiClient } from './src/subsonic/api';
+import md5 from 'md5';
+
+const password = 'test';
+const salt = 'salty';
+const token = md5(password + salt);
+
+const client = new SubsonicApiClient('http://navidrome.home', 'austin', token, salt);
+
+client.ping();
+client.getArtists();
