b1944f7791e8d5f4a1d941ecd1b6f66ef90539d9
austinried <4966622+austinried@users.noreply.github.com>
2021-06-25 10:14:46 +0900

    update reanimated

--- App.tsx
+++ App.tsx
@@ -4,9 +4,6 @@ import { RecoilRoot } from 'recoil';
 import SplashPage from './src/components/SplashPage';
 import RootNavigator from './src/components/navigation/RootNavigator';
 
-import { enableScreens } from 'react-native-screens';
-enableScreens();
-
 const App = () => (
   <RecoilRoot>
     <SplashPage>
