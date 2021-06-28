84cff58741f8d507e27d8df6902dc0455a59f3e2
austinried <4966622+austinried@users.noreply.github.com>
2021-06-28 11:18:48 +0900

    woops deleted an important import

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import 'react-native-get-random-values';
 import { enableScreens } from 'react-native-screens';
 enableScreens();
 
+import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import TrackPlayer from 'react-native-track-player';
